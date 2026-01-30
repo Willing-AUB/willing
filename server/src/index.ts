a254de342d270de3dee3693edf6d4a9ca77679f6
MarcHamamji <hamamjimarc@gmail.com>
2026-01-30 22:00:15 +0200

    Add database connection and migrations, and setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,38 @@
-import express from 'express';
+import express, { type ErrorRequestHandler } from 'express';
+import morgan from 'morgan';
+
+import config from './config.js';
+import api from './api/index.js';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
-        hello: true
-    });
-});
+app.use(morgan('dev'));
 
-app.listen(9090, () => {
-    console.log("Listening on port 9090");
+app.use(api);
+
+// Not found handler
+app.use((req, res, next) => {
+    res.status(404);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    next(error);
+})
+
+// Error handler
+app.use(((err, req, res, next) => {
+    if (res.statusCode === 200) {
+        res.status(500);
+    }
+
+    res.json({
+        message: err.message,
+        stack: config.NODE_ENV === 'production' ? '' : err.stack,
+    });
+}) as ErrorRequestHandler);
+
+app.listen(config.SERVER_PORT, (error?: Error) => {
+    if (error) {
+        console.error("Failed to start server");
+    } else {
+        console.log("Listening on port " + config.SERVER_PORT);
+    }
 });
