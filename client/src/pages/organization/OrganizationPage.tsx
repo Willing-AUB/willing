fb39fb24623dd544504511c5e34dfcd6f19a59af
anjiabdallah <anjiabdallah@gmail.com>
2026-02-06 12:00:51 +0200

    removed some comments

--- client/src/pages/organization/OrganizationPage.tsx
+++ client/src/pages/organization/OrganizationPage.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router';
 function OrganizationPage() {
   return (
     <main className="h-screen flex flex-col">
-      {/* Navbar */}
+      {}
       <div className="navbar bg-base-100 shadow-md">
         <div className="navbar-start">
           <a className="btn btn-ghost text-xl" href="/">
@@ -12,11 +12,10 @@ function OrganizationPage() {
           </a>
         </div>
         <div className="navbar-end">
-          {/* Optional: you can add dropdown or login/logout later */}
+          {}
         </div>
       </div>
-
-      {/* Outlet for nested routes */}
+      {}
       <Outlet />
     </main>
   );
