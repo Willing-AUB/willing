34799919fe39171fff321ded7853fad1a809f142
anjiabdallah <anjiabdallah@gmail.com>
2026-02-17 09:59:13 +0200

    fix imports related to organizationPosting.ts

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express';
 import { setUserJWT, authorizeOnly } from './authorization.js';
 import adminRouter from './routes/admin/index.js';
 import indexRouter from './routes/organization/index.js';
-import postingRouter from './routes/organization/organizationPosting.js';
+import postingRouter from './routes/organization/posting.js';
 import userRouter from './routes/user.js';
 import volunteerRouter from './routes/volunteer.js';
 
