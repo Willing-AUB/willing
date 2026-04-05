35474188d70e8e841525e51777924edb893f5ed9
anjiabdallah <anjiabdallah@gmail.com>
2026-04-05 04:35:13 +0300

    make cards all have the same height

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -42,12 +42,12 @@ function Card({
 }: CardProps) {
   return (
 
-    <div className={`card bg-base-100 shadow-md border border-base-300 ${className}`.trim()}>
-      <div className={`card-body ${padding ? '' : 'p-0'}`}>
+    <div className={`card h-full flex flex-col bg-base-100 shadow-md border border-base-300 ${className}`.trim()}>
+      <div className={`card-body flex-1 flex flex-col ${padding ? '' : 'p-0'}`}>
         {title && (
           <div className={`flex items-center gap-2 ${description ? '' : (children ? 'mb-3' : 'mb-0')}`}>
             { left }
-            <h5 className={`font-bold text-lg inline-flex items-center gap-2 text-${coloredText ? color : ''}`}>
+            <h5 className={`font-bold text-lg inline-flex items-center gap-2 ${coloredText ? `text-${color}` : ''}`}>
               {Icon && <Icon size={17} className={`text-${color}`} />}
               {link
                 ? (
